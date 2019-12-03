5d139a6f1ec1437057597459a6303c2ffc053c1e
AxY480 <18716463@qq.com>
2019-12-04 00:36:23 +0800

    初始表结构

--- view/web/public/left.php
+++ view/web/public/left.php
@@ -3,66 +3,33 @@
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
             <li>
-                <a href="/admin/index"><i class="fa fa-dashboard fa-fw"></i> 欢迎</a>
+                <a href="/admin/index"><i class="fa fa-dashboard fa-fw"></i> 状态</a>
             </li>
             <li>
-                <a href="#"><i class="fa fa-camera fa-fw"></i> 全景任务<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-camera fa-fw"></i> 配置中心<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="/admin/queSta">队列状态</a>
+                        <a href="/admin/queSta">配置列表</a>
                     </li>
                     <li>
-                        <a href="/admin/testUp">测试上传</a>
+                        <a href="/admin/testUp">请求历史</a>
                     </li>
                     <li>
-                        <a href="/admin/jobs">任务列表</a>
-                    </li>
-                    <li>
-                        <a href="/admin/panoPrj">全景项目</a>
+                        <a href="/admin/jobs">推送历史</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
             <li>
-                <a href="#"><i class="fa fa-jsfiddle fa-fw"></i> 服务器<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-jsfiddle fa-fw"></i> 服务中心<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="/admin/ecs">ECS</a>
+                        <a href="/admin/ecs">生产者</a>
                     </li>
                     <li>
-                        <a href="/admin/safeGroup">安全组</a>
+                        <a href="/admin/safeGroup">消费者</a>
                     </li>
                 </ul>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-database fa-fw"></i> 数据库<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="/admin/rds">RDS</a>
-                    </li>
-                    <li>
-                        <a href="/admin/db">Mysql</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-soundcloud fa-fw"></i> Oss<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="/admin/bucket">Bucket</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-link fa-fw"></i> 域名<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="/admin/dlist">Domain</a>
-                    </li>
-                </ul>
-                <!-- /.nav-second-level -->
             </li>
             <li>
                 <a href="#"><i class="fa fa-user-md fa-fw"></i> 管理员<span class="fa arrow"></span></a>
